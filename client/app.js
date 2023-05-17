a7d70190a5738226882a6a5187ca530dfa97d5a1
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-17 21:53:57 +0200

    Add client app.js file

---
+++ client/app.js
@@ -0,0 +1,8 @@
+const loginForm = document.getElementById("welcome-form"),
+  messagesSection = document.getElementById("messages-section"),
+  messagesList = document.getElementById("messages-list"),
+  addMessageForm = document.getElementById("add-messages-form"),
+  userNameInput = document.getElementById("welcome-form"),
+  messageContentInput = document.getElementById("message-content");
+
+let userName;
