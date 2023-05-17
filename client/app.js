39304ae22215893403c1443571598a79f378f199
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-17 22:14:01 +0200

    Add submit event listenet to login form

--- client/app.js
+++ client/app.js
@@ -2,7 +2,23 @@ const loginForm = document.getElementById("welcome-form"),
   messagesSection = document.getElementById("messages-section"),
   messagesList = document.getElementById("messages-list"),
   addMessageForm = document.getElementById("add-messages-form"),
-  userNameInput = document.getElementById("welcome-form"),
+  userNameInput = document.getElementById("username"),
   messageContentInput = document.getElementById("message-content");
 
 let userName;
+
+loginForm.addEventListener("submit", (e) => {
+  login(e);
+});
+
+const login = (e) => {
+  e.preventDefault();
+
+  if (!userNameInput.value) alert("Hello! I am an alert box!!");
+  else {
+    userName = userNameInput.value;
+
+    loginForm.classList.remove("show");
+    messagesSection.classList.add("show");
+  }
+};
