1cb34cfa1b906850f35692e93f3f7e8f2ac186f0
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-19 22:23:00 +0200

    Create new socket connection listener

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const socket = require("socket.io");
+const { messages } = require("./db/db");
+
 const app = express();
 PORT = 8000;
 
-const { messages } = require("./db/db");
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
+
+const io = socket(server);
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "/client")));
 
-app.get("*", (res, req) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("New client! Its id – " + socket.id);
+  socket.on("message", () => {
+    console.log("Oh, I've got something from " + socket.id);
+  });
+  console.log("I've added a listener on message event \n");
 });
