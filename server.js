52cf53bc0a56b9fe6977cf7da4ade77e9fd1eb32
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-19 21:40:37 +0200

    Feature: Server run client app

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 PORT = 8000;
 
+const { messages } = require("./db/db");
+
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
+});
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "/client")));
+
+app.get("*", (res, req) => {
+  res.sendFile(path.join(__dirname, "/client/index.html"));
 });
